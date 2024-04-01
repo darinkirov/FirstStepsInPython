44e47086cd61394882bb153a8249719bb1e71bf3
darinkirov <darinkirovmid@abv.bg>
2024-04-01 16:27:57 +0300

    Basic Syntax, Conditional Statements and Loops - Exercise

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/05_orders.py
@@ -0,0 +1,17 @@
+number_of_orders = int(input())
+price_total = 0
+for orders in range(number_of_orders):
+    price_per_capsule = float(input())
+    days = int(input())
+    capsules_needed_per_day = int(input())
+    if price_per_capsule < 0.01 or price_per_capsule > 100:
+        continue
+    if days <  1 or days > 31:
+        continue
+    if capsules_needed_per_day < 1 or capsules_needed_per_day > 2000:
+        continue
+
+    price = price_per_capsule*days*capsules_needed_per_day
+    price_total += price
+    print(f"The price for the coffee is: ${price:.2f}")
+print(f"Total: ${price_total:.2f}")
---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/06_string_pureness.py
@@ -0,0 +1,18 @@
+def is_pure_string(s):
+    forbidden_chars = {',', '.', '_'}
+    for char in s:
+        if char in forbidden_chars:
+            return False
+    return True
+
+def main():
+    n = int(input("Enter the number of strings: "))
+    for _ in range(n):
+        string = input("Enter a string: ")
+        if is_pure_string(string):
+            print(f"{string} is pure.")
+        else:
+            print(f"{string} is not pure!")
+
+if __name__ == "__main__":
+    main()
